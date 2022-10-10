5a264471da4b06f70fb0a640d2421e3672d7c1aa
Thanos Poulos <athanasios.poulos@gmail.com>
2022-10-10 18:32:27 +0200

    Added language definition

--- auto_subtitle/cli.py
+++ auto_subtitle/cli.py
@@ -26,6 +26,8 @@ def main():
     parser.add_argument("--task", type=str, default="transcribe", choices=[
                         "transcribe", "translate"], help="whether to perform X->X speech recognition ('transcribe') or X->English translation ('translate')")
 
+    parser.add_argument("--language", type=str, default=None, choices=sorted(whisper.LANGUAGES.keys()) + sorted([k.title() for k in whisper.TO_LANGUAGE_CODE.keys()]), help="language spoken in the audio, specify None to perform language detection")
+
     args = parser.parse_args().__dict__
     model_name: str = args.pop("model")
     output_dir: str = args.pop("output_dir")
