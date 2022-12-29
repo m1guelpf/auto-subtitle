553deb92c0420d86a655bab6ed13980b840f4464
root <839540+reikjarloekl@users.noreply.github.com>
2022-12-29 14:16:48 +0000

    Remove wav file after extracting text.

--- auto_subtitle/cli.py
+++ auto_subtitle/cli.py
@@ -103,6 +103,7 @@ def get_subtitles(audio_paths: list, output_srt: bool, output_dir: str, transcri
             write_srt(result["segments"], file=srt)
 
         subtitles_path[path] = srt_path
+        os.remove(audio_path)
 
     return subtitles_path
 
