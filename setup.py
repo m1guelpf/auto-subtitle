f14246f6a4bd0305846f326fd61dab7d6cb81826
JesseBot <jessebot@linux.com>
2023-12-04 13:37:25 +0100

    Update setup.py - add ffmpeg-python to required modules

--- setup.py
+++ setup.py
@@ -8,6 +8,7 @@ setup(
     author="Miguel Piedrafita",
     install_requires=[
         'openai-whisper',
+        'ffmpeg-python'
     ],
     description="Automatically generate and embed subtitles into your videos",
     entry_points={
